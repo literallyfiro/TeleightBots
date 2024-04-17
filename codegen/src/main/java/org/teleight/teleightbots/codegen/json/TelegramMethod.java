f71e1b4b722f0d54d752651cf17998cedad1444b
ImOnlyFire <56583784+ImOnlyFire@users.noreply.github.com>
2024-04-17 10:23:33 +0200

    Add gson annotations to TelegramMethod

--- codegen/src/main/java/org/teleight/teleightbots/codegen/json/TelegramMethod.java
+++ codegen/src/main/java/org/teleight/teleightbots/codegen/json/TelegramMethod.java
@@ -1,13 +1,36 @@
 package org.teleight.teleightbots.codegen.json;
 
+import com.google.gson.annotations.SerializedName;
 import com.squareup.javapoet.TypeName;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public record TelegramMethod(
+        @SerializedName("name")
+        @NotNull
         String name,
+
+        @SerializedName("href")
+        @NotNull
         String href,
+
+        @SerializedName("description")
+        @NotNull
         String[] description,
+
+        @SerializedName("returns")
+        @Nullable
         TypeName[] returns,
+
+        @SerializedName("fields")
+        @Nullable
         TelegramField[] fields,
-        TypeName[] subtype_of,
+
+        @SerializedName("subtype_of")
+        @Nullable
+        TypeName[] subtypeOf,
+
+        @SerializedName("subtypes")
+        @Nullable
         TypeName[] subtypes
 ) {}
